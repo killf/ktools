dcec93aa9df7196f3195474e9afd92e74296bb6f
killf <killf@foxmail.com>
2014-05-08 10:30:06 +0800

    XML序列化和反序列化测试完毕

---
+++ KTools/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ls = new List<string>();
+            ls.Add("Hello world!!!");
+
+            KTools.Serializer.XMLSerializer.Serialize<List<string>>(ls, "d:/1.txt");
+           var l= KTools.Serializer.XMLSerializer.DeSerialize<List<string>>("d:/1.txt");
+            Console.WriteLine(l[0]);
+            Console.ReadLine();
+        }
+    }
+}
